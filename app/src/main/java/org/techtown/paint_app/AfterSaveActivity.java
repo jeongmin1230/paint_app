133a8d9166894f779c41a49eab196fee98513bd3
jeongmin <tmxk0810@gmail.com>
2022-03-23 17:30:28 +0900

    처음 생각한 레이아웃대로 수정
    기능은 아무것도 추가하지 않음

---
+++ app/src/main/java/org/techtown/paint_app/AfterSaveActivity.java
@@ -0,0 +1,14 @@
+package org.techtown.paint_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AfterSaveActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_after_save);
+    }
+}

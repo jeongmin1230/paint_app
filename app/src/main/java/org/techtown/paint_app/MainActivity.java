8fed9c7a514e358b926ddeb10a1840e67c7ecc4b
jeongmin <tmxk0810@gmail.com>
2022-03-24 15:25:58 +0900

    - 텍스트뷰의 background 지정을 통해 색깔을 여러개 나타내고 그 텍스트뷰를 클릭 할 때마다 Toast 메세지가 출력되게 함 - 레이아웃을 조금 더 깔끔하게 수정

--- app/src/main/java/org/techtown/paint_app/MainActivity.java
+++ app/src/main/java/org/techtown/paint_app/MainActivity.java
@@ -1,10 +1,11 @@
-package org.techtown.paint_app;
+ package org.techtown.paint_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +13,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+
+        switch(view.getId()){ // 텍스트뷰를 누를 때마다 switch - case 문으로 버튼 누를 때마다 글자 색이 toast 메세지로 출력됨
+            case R.id.red:
+                Toast.makeText(getApplicationContext(), "click red", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.orange:
+                Toast.makeText(getApplicationContext(), "click orange", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.yellow:
+                Toast.makeText(getApplicationContext(), "click yellow", Toast.LENGTH_SHORT).show();;
+                break;
+            case R.id.green:
+                Toast.makeText(getApplicationContext(), "click green", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.blue:
+                Toast.makeText(getApplicationContext(), "click blue", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.indigo:
+                Toast.makeText(getApplicationContext(), "click indigo", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.purple:
+                Toast.makeText(getApplicationContext(), "click purple", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.pink:
+                Toast.makeText(getApplicationContext(), "click pink", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.white:
+                Toast.makeText(getApplicationContext(), "click white", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.black:
+                Toast.makeText(getApplicationContext(), "click black", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 
     public void onClickSave(View view) { // 이미지 저장 버튼 눌렀을 때 인텐트 이동
